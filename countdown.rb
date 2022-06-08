e86aacd535e5ab4f38cd32041f6d56e188741592
Susan Minott <susan_minott@yahoo.com>
2022-06-08 09:33:05 -0400

    commit tree 84392a2cb56acd3bb74715639367ebbcf1fd1a3b

--- countdown.rb
+++ countdown.rb
@@ -2,3 +2,21 @@
 
 def countdown
 end
+def countdown(int)
+    int = 10
+    while int > 0
+        puts "#{int} SECOND(S)!"
+        int -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+    int = 10
+    while int > 0
+        puts "#{int} SECOND(S)!"
+        int -= 1
+        sleep 1
+    end
+        "HAPPY NEW YEAR!"
+end
